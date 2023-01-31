1cf05af9ec89e07d20da398b5549e73db1431e78
sergio <sergio.moy@gmail.com>
2023-01-31 18:06:18 -0500

    setup-gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -17,7 +17,7 @@ import BuscarPedido from './Presentation/Seguimiento/BuscarPedido';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={<MainPage/>}/>
         <Route path='/recomendaciones' element={<Recomendaciones/>}/>
