fbded9f5529e2eddad3fe336fe1e6e6bb5152b57
L34209 <L34209@lab.aca>
2023-01-31 19:03:21 -0500

    CAMBIOS VinetaPedidos

--- src/Presentation/Pedidos/Componentes/VinetaPedidos.jsx
+++ src/Presentation/Pedidos/Componentes/VinetaPedidos.jsx
@@ -1,43 +1,54 @@
 import useRows from "./hooks/useRows";
 import CardPedidos from "./CardPedidos";
-function ListaPedidos(){
+function ListaPedidos() {
     const listarow = []//Lista de filas
     let listacol = []//Lista de columnas
     const data = useRows();
+
+    data.forEach(function (pedido, index) {
+        if (index % 3 !== 0) {
+            listacol.push(
+                <div className="col-md-4"><CardPedidos pedi={pedido} /></div>
+            )
+            console.log(listacol)
+        } else {
+            if (index !== 0) {
+                listarow.push(
+                    <div className="row">{listacol}
+                    </div>
+                )
+                listacol = []
+            }
+            listacol.push(
+                <div className="col-md-4"><CardPedidos pedi={pedido} /></div>
+            )
+
+            console.log(listacol)
+
+        }
+
+    })
+    if (data.length !== 0){
+        listarow.push(
+            <div className="row">{ listacol }</div>
+        )
+    } 
     
-    data.forEach(function(pedido, index){
-      if(index % 3 !== 0){
-          listacol.push(
-              <div className="col-md-4"><CardPedidos pedi={pedido}/></div>
-          )
-          console.log(listacol)
-      }else{
-          listacol.push(
-              <div className="col-md-4"><CardPedidos pedi={pedido}/></div>
-          )
-          listarow.push(
-              <div className="row">{ listacol } 
-              </div>
-          )
-          console.log(listacol)
-          listacol = []
-      }
-  })
-  /*
-  if(data.length % 2 !== 0){
-      listacol = []
-      listacol.push(
-          <div className="col-md-4"><CardPedidos pedi={data[data.length-1]} />
-          </div>
-      )
-      listarow.push(
-          <div className="row">{ listacol }</div>
-      )
-  }*/
-        return <div>
-          {
+    /*
+    if(data.length % 2 !== 0){
+        listacol = []
+        listacol.push(
+            <div className="col-md-4"><CardPedidos pedi={data[data.length-1]} />
+            </div>
+        )
+        listarow.push(
+            <div className="row">{ listacol }</div>
+        )
+    }*/
+    return <div>
+        {
             listarow
-          }
-        </div>
+        }
+    </div>
 }
 export default ListaPedidos
