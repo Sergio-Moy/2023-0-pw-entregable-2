10b7ec2e91e4e95e6795a19a05363bd338f9bf53
L35207 <L35207@lab.aca>
2023-02-01 19:47:11 -0500

    termine

--- src/Presentation/Registrarpedido/pedidoregistro.jsx
+++ src/Presentation/Registrarpedido/pedidoregistro.jsx
@@ -1,67 +1,83 @@
-import React, { useState } from "react";
-import TopNav from '../Global/TopNav';
-const Pedidoregistro = () => {
-  const [order, setOrder] = useState({
-    name: "",
-    address: "",
-    phone: "",
-  });
+import React, { useState } from 'react';
 
-  const handleInputChange = (event) => {
-    setOrder({ ...order, [event.target.name]: event.target.value });
-  };
+const products = [
+  { id: 1, name: 'Producto 1', price: 100 },
+  { id: 2, name: 'Producto 2', price: 200 },
+  { id: 3, name: 'Producto 3', price: 300 },
+];
+
+function Pedidoregistro({ onSubmit }) {
+  const [selectedProductId, setSelectedProductId] = useState(null);
+  const [quantity, setQuantity] = useState(1);
+  const [address, setAddress] = useState('');
+  const [deliveryDate, setDeliveryDate] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    try {
-      localStorage.setItem("order", JSON.stringify(order));
-      console.log("Pedido guardado en memoria local");
-    } catch (error) {
-      console.error("Error al guardar pedido en memoria local: ", error);
+    if (!selectedProductId) {
+      return;
     }
+
+    const selectedProduct = products.find(
+      (product) => product.id === selectedProductId
+    );
+
+    onSubmit({
+      product: selectedProduct,
+      quantity,
+      address,
+      deliveryDate,
+    });
   };
 
   return (
-  <div>
-    <TopNav category ={3}/>
-    <div className="centrar">
-  <form onSubmit={handleSubmit}>
-    <div>
-      <label htmlFor="name">Nombre:</label>
-      <input
-        type="text"
-        id="name"
-        name="name"
-        value={order.name}
-        onChange={handleInputChange}
-      />
-    </div>
-    <div>
-      <label htmlFor="address">Dirección:</label>
-      <input
-        type="text"
-        id="address"
-        name="address"
-        value={order.address}
-        onChange={handleInputChange}
-      />
-    </div>
-    <div>
-      <label htmlFor="phone">Número de teléfono:</label>
-      <input
-        type="text"
-        id="phone"
-        name="phone"
-        value={order.phone}
-        onChange={handleInputChange}
-      />
-    </div>
-    <button type="submit">Realizar pedido</button>
-  </form>
-</div></div>
-    
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="product">Producto:</label>
+        <select
+          id="product"
+          value={selectedProductId || ''}
+          onChange={(event) =>
+            setSelectedProductId(Number(event.target.value))
+          }
+        >
+          <option value="">Seleccione un producto</option>
+          {products.map((product) => (
+            <option key={product.id} value={product.id}>
+              {product.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label htmlFor="quantity">Cantidad:</label>
+        <input
+          type="number"
+          id="quantity"
+          value={quantity}
+          onChange={(event) => setQuantity(Number(event.target.value))}
+        />
+      </div>
+      <div>
+        <label htmlFor="address">Dirección de entrega:</label>
+        <input
+          type="text"
+          id="address"
+          value={address}
+          onChange={(event) => setAddress(event.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="deliveryDate">Fecha de entrega:</label>
+        <input
+          type="date"
+          id="deliveryDate"
+          value={deliveryDate}
+          onChange={(event) => setDeliveryDate(event.target.value)}
+        />
+      </div>
+      <button type="submit">Realizar pedido</button>
+    </form>
   );
-};
-
+}
 export default Pedidoregistro;
