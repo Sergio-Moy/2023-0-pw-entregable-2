25ccb8c78e6d306ce81198cfb88691f3b43d15ec
Aaron <aaronlivias0412@gmail.com>
2023-02-02 08:48:37 -0500

    Se  agrego cambios a Pedidos, y estilos en la ultima parte

--- src/Presentation/Pedidos/Componentes/CardPedidos.jsx
+++ src/Presentation/Pedidos/Componentes/CardPedidos.jsx
@@ -1,28 +1,29 @@
 function CardPedidos(props){
-    return <div >
+    return <div className="Vinetas">
+        <div className="titulo font-monospace center">
+        {props.pedi.Producto}
+        </div>
+        <br/>
     <div className="estiloo">
         <img src={props.pedi.Imagen} width="300" height="200"/>
     </div>
-    <div align="center">
-        <br/>
-    <dl class="row">
-  <dt class="col-sm-3">Codigo</dt>
-  <dd class="col-sm-9">{props.pedi.Codigo}</dd>
+    <br/>
+    <div align="center" className="padding">
+    <dl className="row">
+  <dt className="col-sm-3">Codigo</dt>
+  <dd className="col-sm-9">{props.pedi.Codigo}</dd>
 
-  <dt class="col-sm-3">Producto</dt>
-  <dd class="col-sm-9">{props.pedi.Producto}</dd>
+  <dt className="col-sm-3">Precio</dt>
+  <dd className="col-sm-9">{props.pedi.Precio}</dd>
 
-  <dt class="col-sm-3">Precio</dt>
-  <dd class="col-sm-9">{props.pedi.Precio}</dd>
+  <dt className="col-sm-3 text-truncate">Cant.</dt>
+  <dd className="col-sm-9">{props.pedi.Cantidad}</dd>
 
-  <dt class="col-sm-3 text-truncate">Cantidad</dt>
-  <dd class="col-sm-9">{props.pedi.Cantidad}</dd>
-
-  <dt class="col-sm-3">Estado</dt>
-  <dd class="col-sm-9">{props.pedi.Estado}</dd>
+  <dt className="col-sm-3">Estado</dt>
+  <dd className="col-sm-9">{props.pedi.Estado}</dd>
   
-  <dt class="col-sm-3 text-truncate">Total</dt>
-  <dd class="col-sm-9">{props.pedi.Cantidad*props.pedi.Precio}</dd>
+  <dt className="col-sm-3 text-truncate">Total</dt>
+  <dd className="col-sm-9">{props.pedi.Cantidad*props.pedi.Precio}</dd>
 </dl>
     </div>
     </div>
