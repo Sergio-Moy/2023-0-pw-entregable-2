0b0ca880f33c098582bb9ef0114643b88535f420
L34313 <L34313@lab.aca>
2023-02-13 18:14:19 -0500

    login restaurante creado

--- src/Presentation/loginrestaurante/loginpage.jsx
+++ src/Presentation/loginrestaurante/loginpage.jsx
@@ -1,50 +1,70 @@
-import { useNavigate } from "react-router-dom"
-import Loginrestaurante from "./loginrestaurante"
-import TopNav from '../Global/TopNav';
-function LoginPage() {
 
-    const navigate = useNavigate()
+import React, { useState } from 'react';
+import './login.css'
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [imagen,setImagen]= useState('');
+  const [error, setError] = useState(null);
 
-    const onLoginOk = function(
-        usuario, password
-    ) {
-        if (usuario === "pw" 
-            && password === "123") {
-
-            const dataUsuario = {
-                username : usuario,
-                password : password
-            }
-
-            // JSON.stringify : convierte objetos js a JSON (string)
-            const dataUsuarioJSON = JSON.stringify(dataUsuario)
-            console.log(dataUsuario)
-            console.log(dataUsuarioJSON)
-            // Guardado en session storage
-            sessionStorage.setItem("DATA_USUARIO", dataUsuarioJSON)
-
-
-            navigate("/2023-0-pw-entregable-2/pedidos", {
-                state : {
-                    username : usuario
-                }
-            })
-        }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:8000/backend/loginrestaurante", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password
+        })
+      });
+      const data = await response.json();
+      console.log(data)
+      setImagen(data.restaurante.imagen)
+      
+      // redirigir al usuario a la página principal
+    } catch (err) {
+      console.log("no ingreso")
+      setError(err.message);
     }
+  };
 
-    return <div>
-        <TopNav category ={3}/>
-        <div className="container">
-    <div className="row">
-        <div className="col"></div>
-        <div className="col">
-            <Loginrestaurante 
-                onLoginOk={ onLoginOk } />
-        </div>
-        <div className="col"></div>
+  return (
+    <div className="login-box">
+        <form>
+        <p>Login</p>
+  <form>
+    <div className="user-box">
+      <input type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+/>
+      <label>Email</label>
     </div>
-</div></div>
-    
-}
+    <div className="user-box">
+      <input type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+/>
+      <label>Password</label>
+    </div>
+    <a onClick={handleSubmit}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
+  <p>Don't have an account? <a href="" className="a2">Sign up!</a></p>
+        </form>
+  
+</div>
+  );
+};
 
-export default LoginPage;
+export default Login;
