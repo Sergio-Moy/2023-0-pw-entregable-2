b7582ecfc54af825e969097ea714199f3067bc26
jazmin <20202689@aloe.ulima.edu.pe>
2023-02-26 16:39:30 -0500

    listo

--- src/Presentation/categorias/nuevacategoria.jsx
+++ src/Presentation/categorias/nuevacategoria.jsx
@@ -1,91 +1,158 @@
-import React, { useState } from "react";
-import TopNav from '../Global/TopNav';
-
+import React, { useEffect, useState } from "react";
+import TopNav from "../Global/TopNav";
 
 const Category = ({ category, onSelect }) => (
   <button onClick={() => onSelect(category.id)}>{category.name}</button>
 );
 
-const Dish = ({ dish }) => <p>{dish.name}</p>;
+const Dish = ({ data }) => (
+  <div
+    className="card col-lg-4"
+    style={{
+      width: "18rem",
+      margin: "0 1rem",
+      minHeight: "20rem",
+      backgroundColor: "white",
+      borderRadius: "10px",
+      boxShadow: "5px 5px 10px rgb(135, 135, 135)",
+    }}
 
-const Restaurant = ({ restaurant }) => {
-  const [selectedCategoryId, setSelectedCategoryId] = useState(null);
+  >
+    <img
+      className="card-img-top"
+      src={data.imagen}
+      style={{ borderRadius: "10px" }}
+    />
+    <div className="card-body text-center">
+      <h6>{data.producto}</h6>
+      <h5>S/{data.precio}</h5>
+      <button
+        style={{
+          backgroundColor: "rgb(200, 200, 200)",
+          borderRadius: "5px",
+          fontSize: "14px",
+          padding: "5px 16px",
+          marginTop: "0.5rem",
+        }}
+      >
+        Añadir al carrito
+      </button>
+    </div>
+  </div>
+);
+
+const Categoria = () => {
+  const [platos, setPlatos] = useState([]);
+  const [restaurantes, setRestaurantes] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const getCategories = async (restauranteId) => {
+    const rawResponse = await fetch(
+      "http://localhost:8000/backend/platosprueba?id=" + restauranteId
+    );
+    await rawResponse.json().then((data) => {
+      setCategories(data.categorias);
+    });
+  };
+
+  const getData = async (categoriaId, restauranteId) => {
+    const rawResponse = await fetch(
+      "http://localhost:8000/backend/platos_10/listar?categoria=" +
+        categoriaId +
+        "&restaurante=" +
+        restauranteId
+    );
+    await rawResponse.json().then((data) => {
+      setPlatos(data.platos_10);
+    });
+  };
+
+  const getRestaurantes = async () => {
+    const rawResponse = await fetch(
+      "http://localhost:8000/backend/obtener_restaurantes"
+    );
+    await rawResponse.json().then((data) => {
+      setRestaurantes(data.restaurantes);
+    });
+  };
+
+  const [id_restaurante, setIdRestaurante] = useState(1);
+  var id_categoria = -1;
+
+  useEffect(() => {
+    getData(-1, 1);
+    getCategories(1);
+    getRestaurantes();
+  }, []);
 
   return (
     <div>
-      <h1>{restaurant.name}</h1>
-      <div>
-        {restaurant.categories.map(category => (
-          <Category
-            key={category.id}
-            category={category}
-            onSelect={id => setSelectedCategoryId(id)}
-          />
-        ))}
-      </div>
-      <hr />
-      {selectedCategoryId ? (
-        <div>
-          <h5>Platos de la categoría seleccionada:</h5>
-          {restaurant.dishes
-            .filter(dish => dish.categoryId === selectedCategoryId)
-            .map(dish => (
-              <Dish key={dish.id} dish={dish} />
-            ))}
+      <TopNav category />
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <p style={{ fontSize: "4rem", fontWeight: "200" }}>CATEGORIAS</p>
+        <div style={{marginBottom: "2rem"}}>
+          <span>Restaurante: </span>
+          <select
+            className="custom-select"
+            onChange={(event) => {
+              setIdRestaurante(event.target.value);
+              id_categoria = -1;
+              getData(-1, event.target.value);
+              getCategories(event.target.value);
+            }}
+          >
+            {restaurantes == [] ? (
+              <div>No hay platos</div>
+            ) : (
+              restaurantes.map((restaurante) => {
+                return (
+                  <option value={restaurante.id}>{restaurante.nombre}</option>
+                );
+              })
+              )}
+          </select>
+          <span style={{padding: "0px 20px"}}/>
+          <span>Categoria: </span>
+          <select
+            className="custom-select"
+            onChange={(event) => {
+              getData(event.target.value, id_restaurante);
+            }}
+          >
+            <option selected value="-1">
+              Todos
+            </option>
+            {categories == [] ? (
+              <div>No hay platos</div>
+            ) : (
+              categories.map((categories) => {
+                return (
+                  <option value={categories.id}>{categories.nombre}</option>
+                );
+              })
+            )}
+          </select>
         </div>
-      ) : (
-        <p>Selecciona una categoría para ver sus platos.</p>
-      )}
+      </div>
+
+      <div className="container">
+        <div className="row justify-content-md-center">
+          {platos == [] ? (
+            <div>No hay platos</div>
+          ) : (
+            platos.map((plato) => <Dish key={plato.id} data={plato} />)
+          )}
+        </div>
+      </div>
     </div>
   );
-};
-
-const Categoria = () => {
-  const restaurants = [
-    {
-      id: 1,
-      name: "Listo",
-      categories: [
-        { id: 1, name: "Desayuno" },
-        { id: 2, name: "Almuerzo" },
-        { id: 3, name: "Cena" }
-      ],
-      dishes: [
-        { id: 1, name: "Combo 1: 1 Café + 1 Triple", categoryId: 1 },
-        { id: 2, name: "Combo 2: Un jugo + 2 Galletas", categoryId: 1 },
-        { id: 3, name: "2 Triples + 2 Gaseosas", categoryId: 2 },
-        { id: 4, name: "Hamburguesa Clasica", categoryId: 2 },
-        { id: 5, name: "Pizza pepperoni grande + 1L de gaseosa", categoryId: 3 },
-        { id: 6, name: "Pizza Americana personal + 1 gaseosa", categoryId: 3 }
-      ]
-    },
-    {
-      id: 2,
-      name: "BEMBOS",
-      categories: [
-        { id: 4, name: "Hamburguesas" },
-        { id: 5, name: "Complementos" },
-      ],
-      dishes: [
-        { id: 7, name: "Hamburguesa de Pollo", categoryId: 4 },
-        { id: 8, name: "Hamburguesa de Carne", categoryId: 4 },
-        { id: 9, name: "Porción de papitas", categoryId: 5 },
-        { id: 10, name: "Extra de queso", categoryId: 5 }
-      ]
-    }
-  ];
-
-  return <div>
-  <TopNav category/>
-    <div className="centrar">
-        <div className="centrar">
-
-      {restaurants.map(restaurant => (
-<Restaurant key={restaurant.id} restaurant={restaurant} />
-))}
-</div>
-    </div>
-    </div>
 };
 
 export default Categoria;
