c24ab86f55aa1d4c7e1feeda0f5c79025bcd9560
sergio <sergio.moy@gmail.com>
2023-02-27 16:46:33 -0500

    bug fix en modificar estados

--- src/Presentation/estados/estados.jsx
+++ src/Presentation/estados/estados.jsx
@@ -1,60 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import TopNav from '../Global/TopNav';
-
-const Pedido = ({ pedido }) => {
-  const [status, setStatus] = useState(pedido.status);
-  
-  const handlePreparadoClick = () => {
-    let bodyObj = {
-      id : pedido.id,
-      estado : 2
-    }
-    let bodySTR = JSON.stringify(bodyObj)
-    fetch("https://restaurantes20194359.azurewebsites.net/backend/cambiarestado", {
-      method : "POST",
-      body : bodySTR
-    })
-    window.location.reload(true)
-  };
-
-  const handleTerminadoClick = () => {
-    let bodyObj = {
-      id : pedido.id,
-      estado : 3
-    }
-    let bodySTR = JSON.stringify(bodyObj)
-    fetch("https://restaurantes20194359.azurewebsites.net/backend/cambiarestado", {
-      method : "POST",
-      body : bodySTR
-    })
-    window.location.reload(true)
-  };
-
-  const estados = ["", "Pendiente", "En preparacion", "Terminado"]
-
-  return (
-        <tr>
-
-            <td>{pedido.id}</td>
-            <td>{pedido.detalles}</td>
-            <td>{estados[pedido.status]}</td>
-            <td><div>
-        {status === 1 && (
-          <>
-            <button onClick={handlePreparadoClick}>En preparación</button>
-            <button onClick={handleTerminadoClick}>Terminado</button>
-          </>
-        )}
-        {status === 2 && (
-            pedido.status=2,
-          <button onClick={handleTerminadoClick}>Terminado</button>
-        )}
-        {status === 3 && (pedido.status=3,<div>Pedido Terminado</div>)}
-      </div></td>
-      
-        </tr>
-  );
-};
 
 function Estados(){
   const [listaPedidos, setListaPedidos] = useState([])
@@ -72,6 +18,62 @@ function Estados(){
   useEffect(function(){
       obtenerPedidos()
   }, [])
+
+  const Pedido = ({ pedido }) => {
+    const navigate = useNavigate()
+    const [status, setStatus] = useState(pedido.status);
+    
+    const handlePreparadoClick = () => {
+      let bodyObj = {
+        id : pedido.id,
+        estado : 2
+      }
+      let bodySTR = JSON.stringify(bodyObj)
+      fetch("https://restaurantes20194359.azurewebsites.net/backend/cambiarestado", {
+        method : "POST",
+        body : bodySTR
+      })
+      obtenerPedidos()
+    };
+  
+    const handleTerminadoClick = () => {
+      let bodyObj = {
+        id : pedido.id,
+        estado : 3
+      }
+      let bodySTR = JSON.stringify(bodyObj)
+      fetch("https://restaurantes20194359.azurewebsites.net/backend/cambiarestado", {
+        method : "POST",
+        body : bodySTR
+      })
+      obtenerPedidos()
+    };
+  
+    const estados = ["", "Pendiente", "En preparacion", "Terminado"]
+  
+    return (
+          <tr>
+  
+              <td>{pedido.id}</td>
+              <td>{pedido.detalles}</td>
+              <td>{estados[pedido.status]}</td>
+              <td><div>
+          {status === 1 && (
+            <>
+              <button onClick={handlePreparadoClick}>En preparación</button>
+              <button onClick={handleTerminadoClick}>Terminado</button>
+            </>
+          )}
+          {status === 2 && (
+              pedido.status=2,
+            <button onClick={handleTerminadoClick}>Terminado</button>
+          )}
+          {status === 3 && (pedido.status=3,<div>Pedido Terminado</div>)}
+        </div></td>
+        
+          </tr>
+    );
+  };
 
   let filas = []
   for(let i = 0; i < listaPedidos.length; i++){
